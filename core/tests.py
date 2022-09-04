be91b36d431f8c9486b1fcc88e08ac0f9003ab6f
Leandro Macato <shadowslayerhero@gmail.com>
2022-09-04 08:37:32 +0800

    imp: add Model testing

--- core/tests.py
+++ core/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Location
 
 # Create your tests here.
 # To test run "python maange.py test"
@@ -7,3 +8,21 @@ class URLTests(TestCase):
     def test_testhomepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code , 200)
+
+
+class ModelTest(TestCase):
+    def test_model_str(self):
+        name = Location.objects.create(name="Manila")
+        self.assertEqual(str(name), "Manila")
+
+    def test_Location_address_none(self):
+        name = Location.objects.create(name="Manila")
+        self.assertEqual(name.address , None)
+
+    def test_Location_lat_is_none(self):
+        name = Location.objects.create(name="Manila")
+        self.assertEqual(name.lat , None)
+
+    def test_Location_long_is_none(self):
+        name = Location.objects.create(name="Manila")
+        self.assertEqual(name.long , None)
